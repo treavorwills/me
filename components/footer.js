db25d0056c519fcd9771c2a96437acac2779d15c
twills <treavorwills@gmail.com>
2023-05-05 23:25:07 -0600

    footer

--- components/footer.js
+++ components/footer.js
@@ -1,9 +1,19 @@
+import { FaGithub, FaLinkedin, FaFileAlt } from 'react-icons/fa';
 
 
 export default function Footer() {
     return (
         <>
         <p>FOOTER</p>
+        <a className="mx-3" href='https://www.linkedin.com/in/treavorwills/' target="_blank">
+                    <FaLinkedin size="3em" color="black" title="Linkedin icon link to Treavor's Linkedin profile" />
+                </a>
+                <a className="mx-3" href='https://github.com/treavorwills' target="_blank">
+                    <FaGithub size="3em" color="black" title="Github icon link to Treavor's Github profile" />
+                </a>
+                <a className="mx-3" href='#' target="_blank">
+                    <FaFileAlt size="3em" color="black" title="Envelop icon link to email Treavor" />
+                </a>
         </>
     );
 }
--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import NavBar from "./navBar";
 import Footer from "./footer";
-import styles from './layout.module.css';
+import styles from "./layout.module.css";
 
 const name = "TW";
 export const siteTitle = "Treavor Wills";
@@ -10,20 +10,20 @@ export const siteTitle = "Treavor Wills";
 export default function Layout({ children, home }) {
   return (
     <>
-      <NavBar />
-      <header>
-        {home ? (
-          <>
-          <h1 className={styles.center}>Treavor Wills (home)</h1>
-          </>
-        ) : (
-          <>
-          <h2 className={styles.center}>Treavor Wills (not home)</h2>
-          </>
-        )}
-      </header>
       <div className={styles.main}>
-      <main>{children}</main>
+        <NavBar />
+        <header>
+          {home ? (
+            <>
+              <h1 className={styles.center}>Treavor Wills (home)</h1>
+            </>
+          ) : (
+            <>
+              <h2 className={styles.center}>Treavor Wills (not home)</h2>
+            </>
+          )}
+        </header>
+        <main >{children}</main>
       </div>
       <Footer />
     </>
